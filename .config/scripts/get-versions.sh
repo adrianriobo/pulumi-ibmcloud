367317439e489649aa329a40a1f40c0db416b84c
Adrian Riobo <ariobolo@redhat.com>
2025-11-19 11:26:51 +0100

    feat: ibm cloud pulumi go provider
    Signed-off-by: Adrian Riobo <ariobolo@redhat.com>

---
+++ .config/scripts/get-versions.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Script to export version environment variables for mise
+
+# This script is referenced by .config/mise.toml
+# It exports version variables that can be used by mise tools
